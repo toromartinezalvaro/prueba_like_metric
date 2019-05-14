62c1a33d81109c61d3d33fcecc05ce0972fd1d96
Sebastián Suárez Valencia <suarez.valencia.sebastian@gmail.com>
2019-05-14 10:42:38 -0500

    Add funcionality to proccess primes

--- src/components/Primes/Locations.jsx
+++ src/components/Primes/Locations.jsx
@@ -2,29 +2,56 @@ import React, { useState, useEffect } from "react";
 import Card, { CardHeader, CardBody } from "../UI/Card/Card";
 import Table from "../UI/Table/Table";
 import Input from "../UI/Input/Input";
+import axios from "axios";
 
 const locations = () => {
   const [headers, setHeaders] = useState([]);
   const [columns, setColumns] = useState([]);
   const [tableData, setTableData] = useState([[]]);
-  
-  useEffect(() => {
-    const response = [
-      [{ id: 1, name: "101", value: null, floor: 1, location: 1 }]
-    ];
 
-    setHeaders(["1", "2", "3", "4"]);
-    setColumns(["1", "2", "3", "4"]);
-    setTableData(response);
+  useEffect(() => {
+    axios
+      .get("http://localhost:1337/api/primes/locations")
+      .then(response => {
+        setHeaders(
+          [...Array(response.data.primes[0].length).keys()].map(o => o + 1)
+        );
+        setColumns(
+          [...Array(response.data.primes.length).keys()].map(
+            o => o + response.data.floors
+          )
+        );
+        setTableData(response.data.primes);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }, []);
+
+  const priceChangeHandler = (id, price) => {
+    axios
+      .put(`http://localhost:1337/api/primes/locations/${id}`, { price: price })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
   const inputs = () => {
     return tableData.map(element => {
       return element.map(prime => (
         <Input
+          mask="currency"
           style={{ width: "75px", fontSize: "16px" }}
-          value={prime.value}
+          value={prime.price}
+          validations={[]}
+          onChange={target => {
+            priceChangeHandler(prime.id, target.value);
+          }}
           placeholder={prime.name}
+          tooltip={prime.name}
         />
       ));
     });
@@ -33,7 +60,7 @@ const locations = () => {
   return (
     <Card>
       <CardHeader>
-        <p>Primas por altura</p>
+        <p>Primas por ubicación</p>
       </CardHeader>
       <CardBody>
         <Table
