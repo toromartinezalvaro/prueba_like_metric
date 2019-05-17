1e97a91850a7d3a690593f8d4b7dc72cc0bcc210
Camilo Vargas <caraham0203@gmail.com>
2019-05-17 09:30:41 -0500

    Detalle update
    Detalle update

---
+++ src/components/Detalle/Adicionales/Adicionales.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import styles from "../Adicionales/Adicionales.module.scss";
+import Card, { CardHeader, CardBody } from "../../UI/Card/Card";
+
+const adicionales = props => {
+    return(
+        <Card>
+            <CardHeader>Adicionales</CardHeader>
+            <CardBody>
+                
+            </CardBody>
+        </Card>
+    )
+}
--- src/containers/Detalle/Detalle.jsx
+++ src/containers/Detalle/Detalle.jsx
@@ -2,10 +2,9 @@ import React, { Component } from "react";
 import Pie from "../../components/Detalle/pie/Pie";
 import Inmueble from "../../components/Detalle/Inmueble/Inmueble";
 import Card, { CardHeader, CardBody } from "../../components/UI/Card/Card";
-import styles from "../Detalle/Detalle.module.scss"
+import styles from "../Detalle/Detalle.module.scss";
 
 export default class Detalle extends Component {
-
   render() {
     return (
       <div>
@@ -14,13 +13,50 @@ export default class Detalle extends Component {
             <p>Inmuebles</p>
           </CardHeader>
           <CardBody>
-            <Inmueble/>
-            <h1>Areas</h1>
+            <Inmueble />
+          </CardBody>
+        </Card>
+        <div style={{ display: "flex" }}>
+          <div style={{ flexGrow: "1" }}>
+            <Card>
+              <CardHeader>
+                <p>Areas</p>
+              </CardHeader>
+              <CardBody>
+                <div className={styles.Container}>
+                  <Pie />
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+          <div style={{ flexGrow: "1" }}>
+            <Card>
+              <CardHeader>
+                <p>Adicionales</p>
+              </CardHeader>
+              <CardBody>
+                <div className={styles.Container}>
+                  <Pie />
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+        <Card>
+          <CardHeader>
+            <p>Inmuebles</p>
+          </CardHeader>
+          <CardBody>
             <div className={styles.Container}>
-              <Pie />
-              <Pie />
+              <div className={styles.Row}>
+                <div className={styles.Cell}>Valor total:</div>
+                <div className={styles.Cell}>Area total:</div>
+              </div>
+              <div className={styles.Row}>
+                <div className={styles.Cell}>Valor mt2:</div>
+                <div className={styles.Cell}>Valor total adicionales:</div>
+              </div>
             </div>
-            <Inmueble></Inmueble>
           </CardBody>
         </Card>
       </div>
