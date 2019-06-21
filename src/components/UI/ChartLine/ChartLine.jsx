c81f7c709034c9f1f3b9a10cdd2dd7ede4a62e3e
Camilo Vargas <caraham0203@gmail.com>
2019-06-21 15:53:22 -0500

    ChartLine Working
    ChartLine Working

--- src/components/UI/ChartLine/ChartLine.jsx
+++ src/components/UI/ChartLine/ChartLine.jsx
@@ -1,75 +1,59 @@
-import React from "react";
-import { Line } from "react-chartjs-2";
-import "chartjs-plugin-annotation";
-import Button from "../Button/Button";
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import 'chartjs-plugin-annotation';
+import Button from '../Button/Button';
 
 class LineChart extends React.Component {
-  state = { arrayData: [] };
+  
 
-  render() {
-    let arrayData = [];
-    let dataGraph = [];
+  
 
-    let labels = [];
-    const dataHelper = [
-      { label: ["Mercado"], borderColor: "" },
-      { label: ["Continua"], borderColor: "#29339B", fill: null },
-      { label: ["Semi-Continua"], borderColor: "#018E42", fill: null },
-      { label: ["Semi-Escalonada"], borderColor: "#EE2E31", fill: null },
-      { label: ["Escalonada"], borderColor: "#29339B", fill: null }
-    ];
+  /* componentDidMount() {
+    const groupFilter = this.findGroup();
 
-    const groups = this.props.groups;
+    const labels = this.makeArrayLabels(groupFilter);
 
-    const groupFilter = groups.find(group => {
-      console.log("Grupo FIND", group.type);
-      return group.type === this.props.groupActive;
+    const arrayDataSets = this.makeArrayDataSets(groupFilter.strategies);
+
+    this.setState({
+      groupActive: this.props.groupActive,
+      data: {
+        labels: labels,
+        datasets: arrayDataSets,
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Estrategias de incrementos',
+        },
+      },
     });
+  }
 
-    if (groupFilter !== undefined) {
-      if (groupFilter.strategies.length > 0) {
-        dataGraph = groupFilter.strategies;
-        if (dataGraph[0] !== undefined) {
-          console.log("dataGraph[0].strategies.length", dataGraph.length);
-          let lengthLabels = dataGraph[0].length;
-          labels = Array.from(Array(lengthLabels), (x, index) => index + 1);
-        }
-      } else {
-        return null;
-      }
-    }
+  componentDidUpdate() {
+    if (this.state.groupActive !== this.props.groupActive) {
+      const groupFilter = this.findGroup();
 
-    console.log("labels antes del push", labels);
-    console.log("dataGraph antes del push", dataGraph);
-    arrayData = [];
+      const labels = this.makeArrayLabels(groupFilter);
 
-    for (let i = 0; i < dataGraph.length; i++) {
-      const dataShow = dataGraph[i];
-      console.log(dataGraph);
-      arrayData.push({
-        data: dataShow,
-        label: dataHelper[i].label,
-        borderColor: dataHelper[i].borderColor,
-        fill: dataHelper[i].fill,
-        lineTension: 0.05
+      const arrayDataSets = this.makeArrayDataSets(groupFilter.strategies);
+
+      this.setState({
+        groupActive: this.props.groupActive,
+        data: {
+          labels: labels,
+          datasets: arrayDataSets,
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Estrategias de incrementos',
+          },
+        },
       });
     }
-
-    console.log("arrayData despues del push", arrayData);
-
-    const data = {
-      labels: labels,
-      datasets: arrayData
-    };
-
-    var options = {
-      title: {
-        display: true,
-        text: "Estrategias de incrementos"
-      }
-    };
-
-    function addData(chart, label, data) {
+  } */
+  /* function addData(chart, label, data) {
       chart.data.labels.push(label);
       chart.data.datasets.forEach(dataset => {
         dataset.data.push(data);
@@ -83,22 +67,24 @@ class LineChart extends React.Component {
         dataset.data.pop();
       });
       chart.update();
-    }
+    } */
 
+  render() {
+    console.log('group p.. ',this.props.currentGroup)
     return (
       <div>
-        <Line data={data} options={options} />
-        <div style={{ textAlign: "center", marginTop: "20px" }}>
-          <h4>Selecciona la estrategia para el {groupFilter.type}</h4>
-          {dataGraph.map((group, index) => {
+        <Line data={{labels: this.props.labels, datasets: this.props.currentGroup}} /* options={this.state.options} */ />
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <h4>Selecciona la estrategia para el {/* this.state.dataGraph.type */}</h4>
+          {/* this.state.dataGraph.map((group, index) => {
             if (index !== 0) {
               return (
                 <Button onClick={() => this.handleClick()}>
-                  {dataHelper[index].label}
+                  {this.state.dataHelper[index].label}
                 </Button>
               );
             }
-          })}
+          }) */}
         </div>
       </div>
     );
--- src/containers/Strategy/Strategy.jsx
+++ src/containers/Strategy/Strategy.jsx
@@ -1,58 +1,133 @@
-import React, { Component } from "react";
-import StrategyServices from "../../services/strategy/StrategyService";
-import Line from "../../components/UI/ChartLine/ChartLine";
-import Card, { CardHeader, CardBody } from "../../components/UI/Card/Card";
-import Button from "../../components/UI/Button/Button";
-let groups = [];
+import React, { Component } from 'react';
+import StrategyServices from '../../services/strategy/StrategyService';
+import Line from '../../components/UI/ChartLine/ChartLine';
+import Card, { CardHeader, CardBody } from '../../components/UI/Card/Card';
+import Button from '../../components/UI/Button/Button';
 export default class Strategy extends Component {
   constructor(props) {
     super(props);
     this.services = new StrategyServices(this);
-    this.chart = React.createRef();
   }
 
-  state = { groupActive: { strategies: [] } };
+  state = {
+    groupActive: { type: 'Tipo 1', strategies: [] },
+    currentGroup: {},
+    groups: [],
+    groupFilter: [],
+    labels: [],
+    staticGroups: [],
+  };
+
+  findGroup = (groups, active) => {
+    return groups.find(group => {
+      return group.type === active.type;
+    });
+  };
+
+  makeArrayLabels = groupFilter => {
+    if (groupFilter !== undefined) {
+      if (groupFilter.strategies.length > 0) {
+        this.setState({ dataGraph: groupFilter.strategies });
+        let dataGraph = groupFilter.strategies;
+        if (dataGraph[0] !== undefined) {
+          let lengthLabels = dataGraph[0].length;
+          return Array.from(Array(lengthLabels), (x, index) => index + 1);
+        }
+      } else {
+        return null;
+      }
+    }
+  };
+
+  makeArrayDataSets = dataGraph => {
+    const dataHelper = [
+      { label: ['Mercado'], borderColor: '' },
+      {
+        label: ['Continua'],
+        borderColor: '#29339B',
+        backgroundColor: '#8A8FC8',
+        fill: null,
+      },
+      {
+        label: ['Semi-Continua'],
+        borderColor: '#018E42',
+        backgroundColor: '#74C197',
+        fill: null,
+      },
+      {
+        label: ['Semi-Escalonada'],
+        borderColor: '#EE2E31',
+        backgroundColor: '#F58D8E',
+        fill: null,
+      },
+      {
+        label: ['Escalonada'],
+        borderColor: '#FFC857',
+        backgroundColor: '#FFE1A3',
+        fill: null,
+      },
+    ];
+    const arrayData = dataGraph.map((line, i) => {
+      return {
+        data: [...line],
+        label: dataHelper[i].label,
+        borderColor: dataHelper[i].borderColor,
+        backgroundColor: dataHelper[i].backgroundColor,
+        fill: dataHelper[i].fill,
+        lineTension: 0.05,
+      };
+    });
+
+    console.log('groups array ', arrayData);
+    return arrayData;
+  };
 
   componentDidMount() {
     this.setState({ isLoading: true });
     this.services
       .getStrategies(this.props.match.params.towerId)
       .then(strategies => {
-        groups = strategies.data;
-        this.setState({ isLoading: false, groupActive: groups[0] });
+        const groupFilter = this.findGroup(strategies.data, strategies.data[0]);
+        const labels = this.makeArrayLabels(groupFilter);
+        const arrayDataSets = this.makeArrayDataSets(groupFilter.strategies);
+        this.setState({
+          isLoading: false,
+          groupActive: strategies.data[0],
+          currentGroup: arrayDataSets,
+          labels: labels,
+          groups: strategies.data,
+        });
       });
   }
 
   handleClick(type) {
-    console.log("chartReference", this.chart.current);
-/*     this.chart.current.remove();
- */    console.log("chartReference", this.chart.current);
-    const groupFilter = groups.find(group => {
+    const groupActive = this.state.groups.find(group => {
       return group.type === type;
     });
-    this.setState({ groupActive: groupFilter });
+    const groupFilter = this.findGroup(this.state.groups, groupActive);
+    const labels = this.makeArrayLabels(groupFilter);
+    const arrayDataSets = this.makeArrayDataSets(groupFilter.strategies);
+    this.setState({
+      currentGroup: arrayDataSets,
+      labels: labels,
+    });
   }
 
   render() {
     return (
       <Card>
-        <div style={{ textAlign: "center", marginBottom: "20px" }}>
-          {groups.map(
-            group => (
-              console.log(group),
-              (
-                <Button onClick={() => this.handleClick(group.type)}>
-                  {group.type}
-                </Button>
-              )
-            )
-          )}
+        <div style={{ textAlign: 'center', marginBottom: '20px' }}>
+          {this.state.groups.map(group => (
+            <Button onClick={() => this.handleClick(group.type)}>
+              {group.type}
+            </Button>
+          ))}
         </div>
         {this.state.groupActive.strategies.length !== 0 ? (
           <Line
             ref={this.chart}
-            groups={groups}
-            groupActive={this.state.groupActive.type}
+            currentGroup={[...this.state.currentGroup]}
+            labels={this.state.labels}
           />
         ) : null}
       </Card>
