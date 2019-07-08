b65ec3004ec3071f21b18ed97a38fcb18e0d4aee
Camilo Vargas Lopera <caraham0203@gmail.com>
2019-07-08 23:32:39 +0000

    ChartLine.jsx edited online with Bitbucket

--- src/components/UI/ChartLine/ChartLine.jsx
+++ src/components/UI/ChartLine/ChartLine.jsx
@@ -4,16 +4,11 @@ import 'chartjs-plugin-annotation';
 import Button from '../Button/Button';
 
 class LineChart extends React.Component {
-  
-
-  
-
-
   render() {
     console.log('group p.. ',this.props.currentGroup)
     return (
       <div>
-        <Line data={{labels: this.props.labels, datasets: this.props.currentGroup}} /* options={this.state.options} */ />
+        <Line data={{labels: this.props.labels, datasets: this.props.currentGroup}} />
       </div>
     );
   }
