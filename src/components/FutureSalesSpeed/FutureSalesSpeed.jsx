ae87e5290f70c75c15af3ff72421fbf989795f30
Caraham <caraham0203@gmail.com>
2019-07-31 16:24:34 -0500

    Add total

--- src/components/FutureSalesSpeed/FutureSalesSpeed.jsx
+++ src/components/FutureSalesSpeed/FutureSalesSpeed.jsx
@@ -1,17 +1,33 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import NumberFormat from 'react-number-format';
 import Card, { CardHeader, CardBody } from '../UI/Card/Card';
 import Input from '../UI/Input/Input';
 import styles from './FutureSalesSpeed.module.scss';
 
-const futureSalesSpeed = ({
+const getTotal = salesSpeeds => salesSpeeds.reduce((current, next) => {
+  current += next;
+  return current;
+}, 0);
+
+
+const FutureSalesSpeed = ({
   salesSpeeds,
   futureSalesSpeedHandler,
   ...rest
 }) => {
-  console.log(salesSpeeds.groups);
+  const [total, setTotal] = useState(0);
+  const [isEmpty, setIsEmpty] = useState(true);
+  const [arraySalesSpeeds, setArraySalesSpeeds] = useState([])
   const { groups } = salesSpeeds;
-  console.log(salesSpeeds);
+  if (groups !== undefined) {
+    if (isEmpty) {
+      const arrayTemporal = groups.map(group => group.futureSalesSpeed)
+      setArraySalesSpeeds(arrayTemporal);
+      setTotal(getTotal(arrayTemporal));
+      setIsEmpty(false);
+    }
+  }
+
   return (
     <Card>
       <CardHeader>
@@ -25,36 +41,43 @@ const futureSalesSpeed = ({
           <h4 className={styles.gridItem}>m² prom</h4>
           <h4 className={styles.gridItem}>Velocidad ventas futura</h4>
           {groups !== undefined
-            ? groups.map(group => (
-                <Fragment key={`fragment ${group.id}`}>
-                  <div className={styles.gridItem}>{group.name.slice(5)}</div>
-                  <div className={styles.gridItem}> {group.units}</div>
-                  <div className={styles.gridItem}>
-                    <NumberFormat
-                      value={group.averagePrice.toFixed(2)}
-                      displayType={'text'}
-                      thousandSeparator={true}
-                      prefix={'$'}
-                    />
-                  </div>
-                  <div className={styles.gridItem}>
-                    {group.averageArea.toFixed(2)}
-                  </div>
-                  <div className={styles.gridItem}>
-                    <Input
-                      validations={[]}
-                      value={group.futureSalesSpeed}
-                      style={{ width: '75px' }}
-                      onChange={target => futureSalesSpeedHandler(group.id, target.value)}
-                    />
-                  </div>
-                </Fragment>
-            ))
+            ? groups.map((group, i) => <Fragment key={`fragment ${group.id}`}>
+                    <div className={styles.gridItem}>{group.name.slice(5)}</div>
+                    <div className={styles.gridItem}> {group.units}</div>
+                    <div className={styles.gridItem}>
+                      <NumberFormat
+                        value={group.averagePrice.toFixed(2)}
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        prefix={'$'}
+                      />
+                    </div>
+                    <div className={styles.gridItem}>
+                      {group.averageArea.toFixed(2)}
+                    </div>
+                    <div className={styles.gridItem}>
+                      <Input
+                        validations={[]}
+                        value={group.futureSalesSpeed}
+                        style={{ width: '75px' }}
+                        onChange={(target) => {
+                          arraySalesSpeeds[i] = Number(target.value);
+                          setTotal(getTotal(arraySalesSpeeds).toFixed(2));
+                          futureSalesSpeedHandler(group.id, target.value);
+                        }}
+                      />
+                    </div>
+                  </Fragment>)
             : null}
+          <div className={styles.gridItem} />
+          <div className={styles.gridItem} />
+          <div className={styles.gridItem} />
+          <h4 className={styles.gridItem}>Total: </h4>
+          <div className={styles.gridItem}>{total}</div>
         </div>
       </CardBody>
     </Card>
   );
 };
 
-export default futureSalesSpeed;
+export default FutureSalesSpeed;
