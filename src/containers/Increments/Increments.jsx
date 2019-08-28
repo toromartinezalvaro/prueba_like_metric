964bc1ee33b36922e4454ecfe844d3ebd309a042
Sebastián Suárez Valencia <suarez.valencia.sebastian@gmail.com>
2019-08-28 16:47:07 -0500

    .0.8.2: gitflow-feature-stash: INSTA-151

--- src/containers/Increments/Increments.jsx
+++ src/containers/Increments/Increments.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Loader from 'react-loader-spinner';
+import commonStyles from '../../assets/styles/variables.scss';
 import Modal from '../../components/UI/Modal/Modal';
 import IncrementsTable from '../../components/Increments/IncrementTable';
 import IncrementsMarket from '../../components/Increments/IncrementsMarket/IncrementsMarket';
 import IncrementsChart from '../../components/Increments/IncrementsChart/IncrementsChart';
 import IncrementsServices from '../../services/increments/IncrementsServices';
 import LoadableContainer from '../../components/UI/Loader';
+import Styles from './Increments.module.scss';
 
 class Increments extends Component {
   constructor(props) {
@@ -20,6 +23,7 @@ class Increments extends Component {
     isLoadingIncrements: false,
     isEmpty: false,
     hidden: true,
+    loadingAPI: true,
   };
 
   componentDidMount() {
@@ -35,15 +39,17 @@ class Increments extends Component {
           increments: response.data.increments,
           market: response.data.market,
           isLoading: false,
+          loadingAPI: false,
         });
       })
       .catch((error) => {
-        this.setState({ isLoading: false });
+        this.setState({ isLoading: false, loadingAPI: false });
         console.error(error);
       });
   };
 
   putSalesSpeed = (id, retentionMonths, index) => {
+    this.setState({ isLoading: true });
     this.services
       .putSalesSpeeds(id, { retentionMonths })
       .then((response) => {
@@ -59,6 +65,7 @@ class Increments extends Component {
     effectiveAnnualInterestRate,
     index,
   ) => {
+    this.setState({ isLoading: true });
     this.services
       .putSuggestedEffectiveAnnualInterestRate(id, {
         effectiveAnnualInterestRate,
@@ -75,6 +82,7 @@ class Increments extends Component {
     if (inventoryUnits === 1 && increment !== collectedIncrement.toFixed(2)) {
       this.setState({ hidden: false });
     } else {
+      this.setState({ isLoading: true });
       this.services
         .putIncrement(this.props.match.params.towerId, {
           groupId: id,
@@ -91,6 +99,7 @@ class Increments extends Component {
 
   toggleModal = () => {
     this.setState((prevState) => ({
+      isLoading: true,
       increments: [],
       hidden: !prevState.hidden,
     }));
@@ -98,29 +107,44 @@ class Increments extends Component {
   };
 
   getPeriodsIncrements = () => {
+    this.setState({ isLoading: true });
     this.services
       .getPeriodsIncrements(this.props.match.params.towerId)
       .then((response) => {
-        this.setState({ graphData: response.data });
+        this.setState({ graphData: response.data, isLoading: false });
       });
   };
 
   putMarketAveragePrice = (averagePrice) => {
-    this.services.putMarketAveragePrice(this.props.match.params.towerId, {
-      averagePrice,
-    });
+    this.setState({ isLoading: true });
+    this.services
+      .putMarketAveragePrice(this.props.match.params.towerId, {
+        averagePrice,
+      })
+      .then(() => {
+        this.setState({ isLoading: false });
+      })
+      .catch((error) => {
+        this.setState({ isLoading: false });
+      });
   };
 
   putMarketAnnualEffectiveIncrement = (anualEffectiveIncrement) => {
-    this.services.putMarketAnualEffectiveIncrement(
-      this.props.match.params.towerId,
-      {
+    this.setState({ loadingAPI: true });
+    this.services
+      .putMarketAnualEffectiveIncrement(this.props.match.params.towerId, {
         anualEffectiveIncrement,
-      },
-    );
+      })
+      .then(() => {
+        this.setState({ isLoading: false });
+      })
+      .catch((error) => {
+        this.setState({ isLoading: false });
+      });
   };
 
   putSuggestedSalesSpeed = (id, retentionMonths, index) => {
+    this.setState({ loadingAPI: true });
     this.services
       .putSuggestedSalesSpeeds(id, { retentionMonths })
       .then((response) => {
@@ -134,6 +158,9 @@ class Increments extends Component {
   render() {
     return (
       <LoadableContainer isLoading={this.state.isLoading}>
+        <div className={Styles.Loader}>
+          <Loader type="ThreeDots" color={commonStyles.mainColor} />
+        </div>
         <IncrementsTable
           data={this.state.increments}
           putIncrement={this.putIncrement}
