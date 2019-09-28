808476b987efea19aa2a205c319428462c77eba4
Sebastián Suárez Valencia <suarez.valencia.sebastian@gmail.com>
2019-09-27 19:02:08 -0500

    Start  qualitative primes view

---
+++ src/components/Primes/QualitativePrimes/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+const QualitativePrimes = () => {
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    
+  })
+
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default QualitativePrimes;
