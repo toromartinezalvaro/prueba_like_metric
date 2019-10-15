089e33d810beddea21827c4a87ffa661e9c94c27
dario vallejo <dariovallejo@MacBook-Pro-de-dario.local>
2019-10-15 12:30:27 -0500

    solving staging

--- src/config/config.js
+++ src/config/config.js
@@ -92,7 +92,7 @@ class Agent {
 }
 
 // export const API_PATH = Server.development.serverUrl;
-export const API_PATH = Server.production.serverUrl;
+export const API_PATH = Server.staging.serverUrl;
 
 const instance = new Agent();
 // export singleton freezed object
