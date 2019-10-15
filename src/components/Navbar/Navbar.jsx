bad503fc66d1a387ff1ceacc80b05dd557c0add3
Jose Luis Hernández Hoyos <joseluis3774060@gmail.com>
2019-10-14 23:31:43 -0500

    changes with material-ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,82 +5,87 @@
  */
 
 import React, { useState } from 'react';
-import Modal from '../../components/UI/Modal/Modal';
-import Accordion from '../UI/Accordion/Accordion';
-import { Link } from 'react-router-dom';
 import styles from './Navbar.module.scss';
-import Select from 'react-select';
+
 import BusinessPatner from '../BusinessPatner/BusinessPatner';
-import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
+import GeneralInformation from '../C_GeneralInfo/GeneralInfo';
+
+import Dialog from '@material-ui/core/Dialog';
+import Fade from '@material-ui/core/Fade';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
 
 const Navbar = () => {
-  const [contractTitleValue, setContractTitleValue] = useState(' ');
+  const [open, setOpen] = useState(false);
+  const [scroll, setScroll] = useState('body');
+  const [fullWidth, setFullWidth] = useState(true);
+  const [maxWidth, setMaxWidth] = useState('md');
+  const [expanded, setExpanded] = useState('GeneralInfo');
 
-  const [contractNumberValue, setContractNumberValue] = useState(' ');
+  const handleCloseExpand = () => {
+    setExpanded('');
+  }
 
-  const [modalState, setModalState] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  }
 
-  const [createNewPatner, setCreateNewPatner] = useState(false);
-
-  const handleContractTitleValue = (target) => {
-    setContractTitleValue(target.value);
-  };
-
-  const handleContractNumber = (target) => {
-    setContractNumberValue(target.value);
-  };
-
-  const handleCreateNewPatner = (isTrue) => {
-    setCreateNewPatner(isTrue);
-  };
+  const handleClose = () => {
+    setOpen(false);
+  }
 
   return (
     <div>
+
       <nav className={styles.navigationBar}>
         <ul className={styles.menuContainer}>
-          <li className={styles.itemList} onClick={() => setModalState(true)}>
+          <li className={styles.itemList} onClick={handleOpen}>
             Contracts
           </li>
         </ul>
       </nav>
 
-      {modalState ? (
-        <Modal style={{ width: '85vw', height: '95vh' }}>
-          <Accordion className={styles.accordion} trigger="General Information">
-            <div className={styles.wrapper}>
-              <h3>Agregar Contrato</h3>
-              <div className={styles.gridOne}>
-                <FormControl className="form-control" variant="outlined">
-                  <OutlinedInput
-                    id="component-outlined"
-                    onChange={handleContractTitleValue}
-                    labelWidth="Titulo del contrato"
-                  />
-                  <InputLabel htmlFor="component-outlined">
-                    Titulo de contrato
-                  </InputLabel>
-                </FormControl>
-              </div>
+      <Dialog
+        className={styles.dialogExpand}
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        fullWidth={fullWidth}
+        maxWidth={maxWidth}>
+        <DialogContentText>
+          <ExpansionPanel expanded={expanded === 'GeneralInfo'} onClick={handleCloseExpand} className={styles.expansionPanel}>
+            <ExpansionPanelSummary
+              aria-controls="generalInformationContent">
+              <Typography className={styles.heading}>Información General</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                <GeneralInformation />
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
 
-              <div className={styles.gridTwo}></div>
-            </div>
+          <ExpansionPanel className={styles.expansionPanel}>
+            <ExpansionPanelSummary
+              aria-controls="generalInformationContent">
+              <Typography className={styles.heading}>Información General</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                <GeneralInformation />
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </DialogContentText>
+      </Dialog>
 
-            <div></div>
-          </Accordion>
-          <Accordion
-            className={styles.accordion}
-            trigger={`Lyfecicle`}
-          ></Accordion>
-          <Accordion
-            className={styles.accordion}
-            trigger={`Billing & Financials`}
-          ></Accordion>
-        </Modal>
-      ) : null}
-    </div>
+    </div >
   );
 };
 
