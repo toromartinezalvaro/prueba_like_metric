935ac612a91ec21ceb137d532b5f496b5f01e8f9
Jose Luis Hernandez <jose.hernandezhoyos@gmail.com>
2019-10-21 16:25:52 -0500

    correction of contract from component to container

---
+++ src/containers/Contract/Contracts.jsx
@@ -0,0 +1,58 @@
+/*
+ * Created by Jcatman on Wed Oct 16 2019
+ *
+ * Copyright (c) 2019 Instabuild
+ */
+
+import React, { Component } from 'react';
+import styles from './Contracts.module.scss';
+import Navbar from '../../components/Contracts/Navbar/Navbar';
+import NewContract from '../../components/Contracts/NewContract/NewContract';
+
+class Contracts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      scroll: 'body',
+      fullWidth: true,
+      maxWidth: 'md',
+      expanded: 'GeneralInfo',
+    };
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div className={styles.Contracts}>
+        <Navbar
+          handleOpen={this.handleOpen}
+          handleClose={this.handleClose}
+          scroll={this.state.scroll}
+          fullWidth={this.state.fullWidth}
+          maxWidth={this.state.maxWidth}
+          expanded={this.state.expanded}
+        />
+        <NewContract
+          handleOpen={this.handleOpen}
+          handleClose={this.handleClose}
+          scroll={this.state.scroll}
+          fullWidth={this.state.fullWidth}
+          maxWidth={this.state.maxWidth}
+          expanded={this.state.expanded}
+          open={this.state.open}
+        />
+      </div>
+    );
+  }
+}
+export default Contracts;
