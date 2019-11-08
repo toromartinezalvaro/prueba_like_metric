b50b5e01d128013a382aacc8dbc9495823b6ec86
Jose Luis Hernandez <jose.hernandezhoyos@gmail.com>
2019-11-08 16:05:03 -0500

    Button for add links

--- src/components/Contracts/NewContract/Content/Attachment/Attachment.jsx
+++ src/components/Contracts/NewContract/Content/Attachment/Attachment.jsx
@@ -1,44 +1,72 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
 import Fab from '@material-ui/core/Fab';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
 import styles from './Attachment.module.scss';
 
-const Attachment = () => {
-  const [multerImage, setMulterImage] = useState();
-  const uploadImage = (e) => {
+class Attachment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      multerImage: '',
+    };
+  }
+
+  uploadImage = (e) => {
     const imageFormObject = new FormData();
 
-    imageFormObject.append("imageName", `insta-image-${Date.now()}`);
-    imageFormObject.append("imageData", e.target.files[0]);
+    imageFormObject.append('imageName', `insta-image-${Date.now()}`);
+    imageFormObject.append('imageData', e.target.files[0]);
 
-    setMulterImage(URL.createObjectURL(e.target.files[0]));
+    this.setState({ multerImage: URL.createObjectURL(e.target.files[0]) });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Card className={styles.card}>
+          <CardContent>
+            <div className={styles.container}>
+              <input
+                type="file"
+                id="upload"
+                className={styles.inputfile}
+                onChange={(e) => this.uploadImage(e)}
+              />
+              <label htmlFor="upload">Adjuntar archivo</label>
+              <img
+                src={this.state.multerImage}
+                className={styles.uploadedImage}
+                alt="uploaded-photo"
+              />
+            </div>
+
+            <div className={styles.row}>
+              <TextField
+                fullWidth
+                className={styles.textField}
+                label="Ingrese Una URL"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                className={styles.buttons}
+              >
+                Guardar URL
+              </Button>
+            </div>
+          </CardContent>
+        </Card>
+      </Fragment>
+    );
   }
-  return (
-    <Fragment>
-      <Card className={styles.card}>
-        <CardContent>
-          <div className={styles.container}>
-            <input type="file" id="upload" className={styles.inputfile} onChange={(e) => uploadImage(e)} />
-            <label htmlFor="upload">Adjuntar archivo</label>
-            <img src={multerImage} className={styles.uploadedImage} alt="uploaded-photo" />
-          </div>
-
-          <TextField
-            fullWidth
-            className={styles.textField}
-            label="Ingrese Una URL"
-            margin="normal"
-            variant="outlined"
-          />
-        </CardContent>
-      </Card>
-    </Fragment>
-  );
 }
 
 export default Attachment;
