328e0cf624d19a3dffec036186ee6b3e142f3b44
Jose Luis Hernandez <jose.hernandezhoyos@gmail.com>
2019-12-20 16:13:40 -0500

    flow of dates

--- src/containers/ContractFlow/ContractFlow.jsx
+++ src/containers/ContractFlow/ContractFlow.jsx
@@ -1,28 +1,52 @@
+/*
+ * Created by Jcatman on Fri Dec 20 2019
+ *
+ * Copyright (c) 2019 Instabuild
+ */
+
 import React, { Component } from 'react';
-import Table from '../../UI/Table/Table';
+import Card, {
+  CardHeader,
+  CardBody,
+  CardFooter,
+} from '../../components/UI/Card/Card';
+import ContractFlowService from '../../services/contractFlow/contractFlowService';
+import TableContractFlow from '../../components/ContractFlow/TableContractFlow';
+import Table from '../../components/UI/Table/Table';
 import Style from './ContractFlow.module.scss';
 
 class ContractFlow extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Contratos: '',
+      information: null,
     };
+    this.service = new ContractFlowService();
   }
 
   componentDidMount() {
-    this.setState({ Contratos: 'Flujo' });
+    this.service
+      .getContractsInformation(this.props.match.params.towerId)
+      .then((response) => {
+        const information = response.data;
+        console.log(information);
+        this.setState({ information });
+      })
+      .catch((error) => console.log(error));
   }
 
   render() {
     return (
-      <Table
-        intersect={'Areas'}
-        headers={['Precio']}
-        columns={this.state.Contratos}
-        data={[0, 1, 2, 3, 4]}
-        maxHeight={{ maxHeight: '36vh' }}
-      />
+      <Card>
+        <CardHeader>
+          <span>Flujo de caja de contratos</span>
+        </CardHeader>
+        <CardBody>
+          {this.state.information && (
+            <TableContractFlow data={this.state.information} />
+          )}
+        </CardBody>
+      </Card>
     );
   }
 }
