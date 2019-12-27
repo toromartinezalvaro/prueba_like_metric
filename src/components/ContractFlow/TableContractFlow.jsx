ef12c62415bb049249f9911d22a4ba2e189efb6b
Jose Luis Hernandez <jose.hernandezhoyos@gmail.com>
2019-12-27 16:06:18 -0500

    table ready

--- src/components/ContractFlow/TableContractFlow.jsx
+++ src/components/ContractFlow/TableContractFlow.jsx
@@ -6,40 +6,84 @@
 
 import React from 'react';
 import moment from 'moment';
+import NumberFormat from 'react-number-format';
 import Table from '../UI/Table/Table';
+import Numbers from '../../helpers/numbers';
 import styles from './TableContractFlow.module.scss';
 
 const TableContractFlow = ({ data }) => {
   const arrayWithTheInformation = data.map((response) => {
+    const initialDate = [];
+    const finalDate = [];
+
     const header = [
-      <div key="Acumulado" className={styles.HeaderCell}>
-        Acumulado
-      </div>,
-      <div key="Proyectado" className={styles.HeaderCell}>
-        Proyectado
-      </div>,
       <div key="Total" className={styles.HeaderCell}>
         Total
       </div>,
     ];
+
+    for (let i = 0; i < data.length; i += 1) {
+      header.push(
+        moment(Number(response.schedulesDate.salesStartDate))
+          .add(i, 'M')
+          .format('MMM YY'),
+      );
+    }
+
+    initialDate.push(
+      moment(Number(response.schedulesDate.salesStartDate)).format(
+        'DD/MM/YYYY',
+      ),
+    );
+    finalDate.push(
+      moment(Number(response.schedulesDate.endOfSalesDate)).format(
+        'DD/MM/YYYY',
+      ),
+    );
+
     return {
-      cells: [{dato: 'dato prueba'}],
+      cells: response.contract.billings.map((row) =>
+        row.map((value, i) => (
+          <div
+            className={`${styles.Cell} ${i === 2 && styles.CellRigth}`}
+            key={styles.Cell + i}
+          >
+            {
+              <NumberFormat
+                value={Numbers.toFixed(value)}
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix={'$'}
+              />
+            }
+          </div>
+        )),
+      ),
       header,
-      name: [{dato: 'dato prueba'}],
+      name: response.contract.title,
+      columns: [response.contract.title],
+      initialDate,
+      finalDate,
     };
   });
 
   return arrayWithTheInformation.map((dataTable, i) => (
     <div key={i} className={styles.AccordionContainer}>
+      <div className={styles.dates}>
+        <div className={styles.initialDate}>
+          <h4>Fecha inicio:</h4>
+          <span className={styles.date}>{dataTable.initialDate}</span>
+        </div>
+        <div className={styles.finalDate}>
+          <h4>Fecha final:</h4>
+          <span className={styles.date}>{dataTable.finalDate}</span>
+        </div>
+      </div>
       <Table
         intersect={''}
         headers={dataTable.header}
         columnsMinWidth={true}
-        columns={[
-          'Cuotas Iniciales Fiducia',
-          'Cuotas Iniciales Disponibles',
-          'Entregas',
-        ]}
+        columns={dataTable.columns}
         data={dataTable.cells}
         maxHeight={{ maxHeight: '36vh' }}
       />
