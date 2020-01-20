17f29c66452b07f1f60b333ea311c8cab67a5f36
Sebastián Suárez Valencia <suarez.valencia.sebastian@gmail.com>
2020-01-20 10:48:16 -0500

    front fixes

--- src/components/Client/SearchOrNew/SearchForm/index.jsx
+++ src/components/Client/SearchOrNew/SearchForm/index.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 import Styles from './SearchClientForm.module.scss';
+
+const SEARCH_NAME = 'name';
+const SEARCH_DOCUMENT = 'document';
+const SEARCH_PROPERTY = 'property';
 
 const SearchClientForm = ({ client, handleChange, isEditing }) => {
   return (
     <div className={Styles.Container}>
+      <div>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">Buscar por:</FormLabel>
+          <RadioGroup
+            aria-label="Tipo de busqueda"
+            name="Tipo de busqueda"
+            onChange={handleChange('searchType')}
+            row
+          >
+            <FormControlLabel
+              value={SEARCH_NAME}
+              control={<Radio />}
+              label="Nombre"
+            />
+            <FormControlLabel
+              value={SEARCH_DOCUMENT}
+              control={<Radio />}
+              label="Cédula "
+            />
+            <FormControlLabel
+              value={SEARCH_PROPERTY}
+              control={<Radio />}
+              label="Número de apartamento"
+            />
+          </RadioGroup>
+        </FormControl>
+      </div>
       <TextField
         className={Styles.TextField}
         disabled={isEditing}
