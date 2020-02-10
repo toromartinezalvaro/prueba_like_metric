cd66567b20ebb6d43e0a688c97f0353e134aa5c5
Sebastián Suárez Valencia <suarez.valencia.sebastian@gmail.com>
2020-02-10 14:53:57 -0500

    Change create behaviour
    - Add loading state to the create/update button
    - Now the dialog does not close automatically when a client is created/updated, it informs the user and show a new button to go to the sales room

--- src/components/Client2/FormDialog/actions.js
+++ src/components/Client2/FormDialog/actions.js
@@ -2,6 +2,10 @@ export const FETCH_ADD_TO_TOWER__START = 1;
 export const FETCH_ADD_TO_TOWER__SUCCESS = 2;
 export const FETCH_ADD_TO_TOWER__FAILURE = 3;
 export const RESTART_STATE = 4;
+export const CLIENT_REQUEST__START = 5;
+export const CLIENT_REQUEST__SUCCESS = 6;
+export const CLIENT_REQUEST__FAILURE = 7;
+export const CREATED_CLIENT = 8;
 
 export const fetchAddToTowerStart = () => ({
   type: FETCH_ADD_TO_TOWER__START,
@@ -17,4 +21,21 @@ export const fetchAddToTowerFailure = () => ({
 
 export const restartState = () => ({
   type: RESTART_STATE,
+});
+
+export const clientRequestStart = () => ({
+  type: CLIENT_REQUEST__START,
+});
+
+export const clientRequestSuccess = () => ({
+  type: CLIENT_REQUEST__SUCCESS,
+});
+
+export const clientRequestFailure = () => ({
+  type: CLIENT_REQUEST__FAILURE,
+});
+
+export const createdClient = (id) => ({
+  type: CREATED_CLIENT,
+  payload: id,
 });
