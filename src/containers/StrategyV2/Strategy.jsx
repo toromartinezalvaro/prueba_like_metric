09fbe7b4dd2856a2b6409623194ab448392be65f
Caraham <aestheticlilca@gmail.com>
2020-04-01 00:55:15 -0500

    maoOfStrategies

--- src/containers/StrategyV2/Strategy.jsx
+++ src/containers/StrategyV2/Strategy.jsx
@@ -1,18 +1,54 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo, useReducer, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Box from '@material-ui/core/Box';
 import { Typography } from '@material-ui/core';
+import _ from 'lodash';
 import Settings from '../../components/StrategyV2/Settings';
 import Widgets from '../../components/StrategyV2/Widgets';
 import Overviews from '../../components/StrategyV2/Overviews';
 import { fetchDataSuccess } from './actions';
+import reducer, { initialState } from './reducer';
+import IncrementServices from '../../services/incrementsV2/incrementsService';
 
 const Strategy = ({ validGroup, onFetchedData }) => {
+  const services = new IncrementServices();
+
+  const helper = [
+    'mercado',
+    'continua',
+    'semicontinua',
+    'semiescalonada',
+    'escalonada',
+  ];
+
   useEffect(() => {
     async function fetch() {
       try {
-        //const response = await services.getData(useHistory);
-        //onFetchedData(null);
+        const response = await services.getIncrementsAndStrategy(
+          '86905mrNYjuVx8X3B1dASdnANO6Y00c50j9KMW8JtMZy25R8VpiVrdyVNGAw',
+        );
+
+        const groupsStrategy = [];
+
+        response.data.increments.forEach((group, indexGroup) => {
+          groupsStrategy.push({
+            id: group.id,
+            lines: _.times(
+              group.strategies[0].increments.length,
+              _.constant({}),
+            ),
+          });
+          group.strategies.forEach((strategy, indexStrategy) => {
+            strategy.increments.forEach((line, indexLine) => {
+              groupsStrategy[indexGroup].lines[indexLine][
+                helper[indexStrategy]
+              ] = line;
+            });
+          });
+        });
+
+        /*         onFetchedData();
+         */
       } catch (error) {
         console.error(error);
       }
