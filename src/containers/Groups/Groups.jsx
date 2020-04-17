e1b6199e3a958d981bcb241efec95fdf31f98901
Jose Luis Hernandez <joseluis3774060@gmail.com>
2020-04-17 13:10:32 -0500

    Steps on ready

--- src/containers/Groups/Groups.jsx
+++ src/containers/Groups/Groups.jsx
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Step,
+  Stepper,
+  StepLabel,
+  StepConnector,
+  Card,
+  CardContent,
+} from '@material-ui/core';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import StepIcon from '../../components/Groups/StepIcon';
 import withDefaultLayout from '../../HOC/Layouts/Default/withDefaultLayout';
 
 class Groups extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeStep: 0,
+      steps: ['Grupos', 'Items'],
+    };
+  }
+
+  handleStep = (activeStep) => () => {
+    this.setState({
+      activeStep,
+    });
+  };
+
+  stepsContent = (step) => {
+    switch (step) {
+      case 0:
+        return 'TEXTO PRUEBA GRUPO';
+      case 1:
+        return 'TEXTO PRUEBA ITEM';
+      default:
+        return 'NO DISPONIBLE';
+    }
+  };
+
   render() {
     return (
-      <div>
-        <h1>helloWorld</h1>
-      </div>
+      <React.Fragment>
+        <Stepper
+          alternativeLabel
+          nonLinear
+          activeStep={this.state.activeStep}
+        >
+          {this.state.steps.map((label, index) => (
+            <Step key={label}>
+              <StepLabel
+                onClick={this.handleStep(index)}
+                StepIconComponent={StepIcon}
+              >
+                {label}
+              </StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <Card>
+          <CardContent>{this.stepsContent(this.state.activeStep)}</CardContent>
+        </Card>
+      </React.Fragment>
     );
   }
 
