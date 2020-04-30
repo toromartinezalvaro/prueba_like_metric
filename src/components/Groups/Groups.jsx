cad3bca6bf75de242f6b241be5fe15001add9e3a
Sebastián Suárez Valencia <suarez.valencia.sebastian@gmail.com>
2020-04-30 12:22:20 -0500

    Update to smaller components

--- src/components/Groups/Groups.jsx
+++ src/components/Groups/Groups.jsx
@@ -1,205 +1,56 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Button,
-  TextField,
-  Paper,
-  Icon,
-  Fab,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from '@material-ui/core';
-/* import {
-  Grid,
-  Table,
-  TableHeaderRow,
-} from '@devexpress/dx-react-grid-material-ui'; */
-
+import Paper from '@material-ui/core/Paper';
+import Icon from '@material-ui/core/Icon';
+import Fab from '@material-ui/core/Fab';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import GroupItem from './Item';
 import styles from './Groups.module.scss';
 
 const Group = ({ groups, createOrUpdateGroup, deleteGroup }) => {
-  const [group, setGroup] = useState(undefined);
-  const [groupList, setGroupList] = useState([]);
-  const [columns] = useState([{ name: 'groups', title: 'Grupos' }]);
-  const [buttonDisabled, setButtonDisabled] = useState(false);
-  const [rows, setRows] = useState([]);
-
-  useEffect(() => {
-    const lockedGroups = groups.map((groupItem) => {
-      return { ...groupItem, disabled: true, hoverMouse: false };
-    });
-    setGroupList(lockedGroups);
-  }, [groups]);
-
-  const changeGroupItem = (index) => (element) => {
-    const groupTemp = [...groupList];
-    const selectedGroup = groupTemp[index];
-    const currentGroup = {
-      ...selectedGroup,
-      categoryName: element.target.value,
-    };
-    groupTemp[index] = currentGroup;
-    setGroup(groupTemp);
-  };
-
-  const addFieldToGroup = (index) => () => {
-    const added = [...(group || groupList)];
-    const selectedGroup = added[index];
-    added[index] = { ...selectedGroup, disabled: true };
-    createOrUpdateGroup(added[index]);
-    setButtonDisabled(false);
-    setGroupList(added);
-    setGroup(undefined);
-  };
-
-  const deleteFieldFromGroup = (id) => () => {
-    setGroupList((prevState) => {
-      const checker = prevState.filter((list) => list.id !== id);
-      console.log(checker);
-      return checker;
-    });
-    /* const validation = groupListWithDeletedItem.find(
-      (item) => item.disabled === false || item.disabled === undefined,
-    );
-    if (validation) {
-      setButtonDisabled(true);
-    } else {
-      setButtonDisabled(false);
-    } */
-  };
-
-  const disableFieldEdit = (index) => () => {
-    const listInstance = [...groupList];
-    listInstance[index] = { ...listInstance[index], disabled: true };
-    setGroupList(listInstance);
-  };
-  const editFieldToGroup = (groupName, index) => () => {
-    const groupListItemToEdit = [...groupList];
-    let selectedItemToEdited = groupListItemToEdit.find(
-      (element) => element.categoryName === groupName,
-    );
-    selectedItemToEdited = { ...selectedItemToEdited, disabled: false };
-    groupListItemToEdit[index] = selectedItemToEdited;
-    setGroupList(groupListItemToEdit);
-  };
-  const hoverMouseAction = (index, visible) => {
-    const added = [...(group || groupList)];
-    const selectedGroup = added[index];
-    added[index] = { ...selectedGroup, hoverMouse: visible };
-    setGroupList(added);
-  };
-  const displayGroupList = (group) => {
-    return group.map((groupItem, index) => {
-      const groupComponent = (
-        <div className={styles.listContainer} key={index}>
-          <TextField
-            label="Nombre del grupo"
-            variant="outlined"
-            defaultValue={groupItem.categoryName}
-            disabled={groupItem.disabled}
-            onChange={changeGroupItem(index)}
-            classes={{ root: styles.texfieldOptions }}
-            fullWidth
-            onMouseEnter={() => hoverMouseAction(index, true)}
-            onMouseLeave={() => hoverMouseAction(index, false)}
-          />
-          {groupItem.hoverMouse && (
-            <>
-              <Button
-                color="primary"
-                onMouseEnter={() => hoverMouseAction(index, true)}
-                onMouseLeave={() => hoverMouseAction(index, false)}
-                onClick={
-                  groupItem.disabled
-                    ? editFieldToGroup(groupItem.categoryName, index)
-                    : addFieldToGroup(index)
-                }
-              >
-                <Icon
-                  className={groupItem.disabled ? 'fas fa-pen' : 'fas fa-check'}
-                />
-              </Button>
-              <Button
-                onMouseEnter={() => hoverMouseAction(index, true)}
-                onMouseLeave={() => hoverMouseAction(index, false)}
-                color="secondary"
-                onClick={
-                  groupItem.disabled
-                    ? deleteFieldFromGroup(groupItem.id)
-                    : disableFieldEdit(groupItem.id)
-                }
-              >
-                <Icon className="fas fa-times" />
-              </Button>
-            </>
-          )}
-        </div>
-      );
-      const object = { groups: groupComponent };
-      return object;
-    });
-  };
-  useEffect(() => {
-    setRows(displayGroupList(groupList));
-  }, [groupList]);
-  const generateFieldGroup = () => {
-    const groupArray = groupList;
-    const validation = groupArray.find(
-      (item) => item.disabled === false || item.disabled === undefined,
-    );
-    if (validation) {
-      setButtonDisabled(true);
-    } else {
-      const index = groupList.length === 0 ? 0 : groupList.length;
-      const currentGroup = [
-        ...groupArray,
-        { index, categoryName: null, disabled: false },
-      ];
-      setButtonDisabled(true);
-      setGroupList(currentGroup);
-    }
-  };
   return (
-    <>
-      <div className={styles.container}>
-        <Paper classes={{ root: styles.container }}>
-          {/* <Grid rows={rows} columns={columns}>
-            <Table />
-            <TableHeaderRow />
-          </Grid> */}
-          <TableContainer component={Paper}>
-            <Table className={styles.table}>
-              <TableHead>
-                <TableRow>
-                  <TableCell align="center">GRUPOS</TableCell>
+    <div className={styles.container}>
+      <Paper classes={{ root: styles.container }}>
+        <TableContainer component={Paper}>
+          <Table className={styles.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">GRUPOS</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.id}>
+                  {groupList.map((group) => (
+                    <TableRow key={group.id}>
+                      <TableCell align="right">
+                        <GroupItem group={group} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                  <TableCell align="right">{row.groups}</TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow key={row.id}>
-                    <TableCell align="right">{row.groups}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Paper>
-        <div className={styles.actionsContainer}>
-          <Fab
-            color="primary"
-            classes={{ root: styles.fabButtonContainer }}
-            disabled={buttonDisabled}
-            onClick={generateFieldGroup}
-          >
-            <Icon className={`fas fa-plus ${styles.fabButton}`} />
-          </Fab>
-        </div>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+      <div className={styles.actionsContainer}>
+        <Fab
+          color="primary"
+          classes={{ root: styles.fabButtonContainer }}
+          disabled={buttonDisabled}
+          onClick={generateFieldGroup}
+        >
+          <Icon className={`fas fa-plus ${styles.fabButton}`} />
+        </Fab>
       </div>
-    </>
+    </div>
   );
 };
 
