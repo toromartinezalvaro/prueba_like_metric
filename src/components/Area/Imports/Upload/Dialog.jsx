45c9983e58cfabed3f6f8d4d39da3e67447002ec
Sebastián Suárez <suarez.valencia.sebastian@gmail.com>
2020-05-08 17:06:58 -0500

    Add loading screen

--- src/components/Area/Imports/Upload/Dialog.jsx
+++ src/components/Area/Imports/Upload/Dialog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
@@ -10,28 +10,47 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import ImportServices from '../../../../services/imports';
-import { closeDialog } from './actions';
+import {
+  closeDialog,
+  changeFile,
+  startApiFetch,
+  stopApiFetch,
+} from './actions';
+import Loader from '../../../UI2/Loader';
 
 const services = new ImportServices();
 
-function Dialog({ open, onCloseHandler }) {
+function Dialog({
+  open,
+  file,
+  loading,
+  onCloseHandler,
+  onSelectFile,
+  onApiFetchStart,
+  onApiFetchEnd,
+}) {
   const { enqueueSnackbar } = useSnackbar();
   const { towerId } = useParams();
-  const [file, setFile] = useState(null);
 
   const onFileUpload = async () => {
+    onApiFetchStart();
     try {
       const formData = new FormData();
       formData.append('file', file);
       await services.postSchema(towerId, formData);
-      enqueueSnackbar('Plantilla cargada correctamente', { variant: 'success' });
+      onCloseHandler();
+      onSelectFile(null);
+      enqueueSnackbar('Plantilla cargada correctamente', {
+        variant: 'success',
+      });
     } catch (error) {
       enqueueSnackbar(error.response.data.message, { variant: 'error' });
     }
+    onApiFetchEnd();
   };
 
   const onChangeHandler = (event) => {
-    setFile(event.target.files[0]);
+    onSelectFile(event.target.files[0]);
   };
 
   return (
@@ -39,25 +58,27 @@ function Dialog({ open, onCloseHandler }) {
       <DialogTitle>Cargar plantilla</DialogTitle>
       <DialogContent>
         <DialogContentText>Agregue la plantilla ya completa.</DialogContentText>
-        <input
-          name="file"
-          accept="xlsx/*"
-          hidden
-          id="raised-button-file"
-          type="file"
-          onChange={onChangeHandler}
-        />
-        <label htmlFor="raised-button-file">
-          <Button
-            variant="contained"
-            color="primary"
-            component="span"
-            disableElevation
-            fullWidth
-          >
-            {file ? file.name : 'Seleccionar plantilla'}
-          </Button>
-        </label>
+        <Loader isLoading={loading}>
+          <input
+            name="file"
+            accept="xlsx/*"
+            hidden
+            id="raised-button-file"
+            type="file"
+            onChange={onChangeHandler}
+          />
+          <label htmlFor="raised-button-file">
+            <Button
+              variant="contained"
+              color="primary"
+              component="span"
+              disableElevation
+              fullWidth
+            >
+              {file ? file.name : 'Seleccionar plantilla'}
+            </Button>
+          </label>
+        </Loader>
       </DialogContent>
       <DialogActions>
         <Button size="small" color="primary" onClick={onCloseHandler}>
@@ -78,15 +99,25 @@ function Dialog({ open, onCloseHandler }) {
 
 const mapStateToProps = (state) => ({
   open: state.areas.open,
+  file: state.areas.file,
+  loading: state.areas.loading,
 });
 
 const mapDispatchToProps = {
   onCloseHandler: closeDialog,
+  onSelectFile: changeFile,
+  onApiFetchStart: startApiFetch,
+  onApiFetchEnd: stopApiFetch,
 };
 
 Dialog.propTypes = {
   open: PropTypes.bool.isRequired,
+  file: PropTypes.any.isRequired,
+  loading: PropTypes.bool.isRequired,
   onCloseHandler: PropTypes.func.isRequired,
+  onSelectFile: PropTypes.func.isRequired,
+  onApiFetchStart: PropTypes.func.isRequired,
+  onApiFetchEnd: PropTypes.func.isRequired,
 };
 
 export default connect(
--- src/components/Area/Imports/Upload/actions.js
+++ src/components/Area/Imports/Upload/actions.js
@@ -1,6 +1,8 @@
 export const UPLOAD_DIALOG_OPEN = 'UPLOAD_DIALOG_OPEN';
-
 export const UPLOAD_DIALOG_CLOSE = 'UPLOAD_DIALOG_CLOSE';
+export const UPLOAD_DIALOG_SELECT_FILE = 'UPLOAD_DIALOG_SELECT_FILE';
+export const UPLOAD_DIALOG_API_FETCH_START = 'UPLOAD_DIALOG_API_FETCH_START';
+export const UPLOAD_DIALOG_API_FETCH_END = 'UPLOAD_DIALOG_API_FETCH_END';
 
 export const openDialog = () => ({
   type: UPLOAD_DIALOG_OPEN,
@@ -8,4 +10,17 @@ export const openDialog = () => ({
 
 export const closeDialog = () => ({
   type: UPLOAD_DIALOG_CLOSE,
+});
+
+export const changeFile = (file) => ({
+  type: UPLOAD_DIALOG_SELECT_FILE,
+  payload: file,
+});
+
+export const startApiFetch = () => ({
+  type: UPLOAD_DIALOG_API_FETCH_START,
+});
+
+export const stopApiFetch = () => ({
+  type: UPLOAD_DIALOG_API_FETCH_END,
 });
