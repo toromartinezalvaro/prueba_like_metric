6685d4a23965c0b5150446d9a11e2d37bd1355fd
Jose Luis Hernandez <joseluis3774060@gmail.com>
2020-05-29 16:11:30 -0500

    COMMENTS

--- src/components/Groups/Imports/Upload/reducer.js
+++ src/components/Groups/Imports/Upload/reducer.js
@@ -1,9 +1,9 @@
 import {
-  UPLOAD_DIALOG_CLOSE,
-  UPLOAD_DIALOG_OPEN,
-  UPLOAD_DIALOG_SELECT_FILE,
-  UPLOAD_DIALOG_API_FETCH_START,
-  UPLOAD_DIALOG_API_FETCH_END,
+  UPLOAD_GROUP_DIALOG_OPEN,
+  UPLOAD_GROUP_DIALOG_CLOSE,
+  UPLOAD_GROUP_DIALOG_SELECT_FILE,
+  UPLOAD_GROUP_DIALOG_API_FETCH_START,
+  UPLOAD_GROUP_DIALOG_API_FETCH_END,
 } from './actions';
 
 const initialState = {
@@ -14,15 +14,15 @@ const initialState = {
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-    case UPLOAD_DIALOG_OPEN:
+    case UPLOAD_GROUP_DIALOG_OPEN:
       return { ...state, open: true };
-    case UPLOAD_DIALOG_CLOSE:
+    case UPLOAD_GROUP_DIALOG_CLOSE:
       return { ...state, open: false };
-    case UPLOAD_DIALOG_SELECT_FILE:
+    case UPLOAD_GROUP_DIALOG_SELECT_FILE:
       return { ...state, file: payload };
-    case UPLOAD_DIALOG_API_FETCH_START:
+    case UPLOAD_GROUP_DIALOG_API_FETCH_START:
       return { ...state, loading: true };
-    case UPLOAD_DIALOG_API_FETCH_END:
+    case UPLOAD_GROUP_DIALOG_API_FETCH_END:
       return { ...state, loading: false };
     default:
       return state;
