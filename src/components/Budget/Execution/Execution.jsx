9ed3da92dce89734c521604df00bcea9393eee46
Darius V <dario.vallejo@rappi.com>
2020-07-31 15:34:17 -0500

    solving INSTA-804 crash

--- src/components/Budget/Execution/Execution.jsx
+++ src/components/Budget/Execution/Execution.jsx
@@ -4,16 +4,18 @@ import LastMonthExecution from './LastMonth';
 import CurrentMonthExecution from './CurrentMonth';
 
 const Execution = () => {
-  const data = useSelector((state) => state.budget.chart.data);  
+  const data = useSelector((state) => state.budget.chart.data);
 
+  if (!data) {
+    return null;
+  }
   return data.length > 0 &&
     data[data.length - 1].estimationAccumulated !== 0 ? (
     <>
       <LastMonthExecution />
       <CurrentMonthExecution />
     </>
-  ) :
-  null;
+  ) : null;
 };
 
 export default Execution;
